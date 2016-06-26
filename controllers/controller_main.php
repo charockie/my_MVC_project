c03aca87560c311484ac77e045e9e711d00a5172
charockie <basman9911@gmail.com>
2016-06-26 21:02:14 +0300

    ref

--- controllers/controller_main.php
+++ controllers/controller_main.php
@@ -6,20 +6,12 @@ class Controller_main extends controller
 
     public function action_index()
     {
-
-        $countries = new countries();
-        $data = $countries->selectAll();
-
-        $this->view->generate('registration/index.php', $data);
+        $this->view->generate('registration/index.php');
     }
 
     public function action_registration()
     {
         echo '<pre>';
         var_dump($_POST);
-    }
-    
-    public function action_hello() {
-        echo '2. hello this is Main controller, hello action!';die;
     }
 }
